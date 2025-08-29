a0d339b2587fb3b943dc031e681f82e533c3b705
lance nguyen <lancesudo@gmail.com>
2025-08-29 02:03:56 -0700

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/payment32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/payment16.png" />
+      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
